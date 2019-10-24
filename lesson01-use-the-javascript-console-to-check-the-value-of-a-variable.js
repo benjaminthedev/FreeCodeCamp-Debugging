6de948a7d89a280ead3bfd72272bc23696121347
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-10-24 14:12:59 +0100

    new lessons added

---
+++ lesson01-use-the-javascript-console-to-check-the-value-of-a-variable.js
@@ -0,0 +1,11 @@
+//use-the-javascript-console-to-check-the-value-of-a-variable
+
+let a = 5;
+let b = 1;
+a++;
+// Add your code below this line
+
+
+let sumAB = a + b;
+console.log(sumAB);
+console.log(a);
